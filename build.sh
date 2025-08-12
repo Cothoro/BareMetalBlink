f578230a1a8b3d7abdb6232e6364c871273849bd
Colton <coltontroe@hotmail.com>
2025-08-11 20:00:04 -0500

    Move main logic to a simple c file.

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ set -xe
 mkdir -p build/
 
 arm-none-eabi-as -mcpu=cortex-m4 -mthumb -o build/main.o main.s
-arm-none-eabi-ld -nostdlib -T stm32f429.ld -o build/main.elf build/main.o
+arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -c -ffreestanding -o build/main_c.o main.c
+arm-none-eabi-ld -nostdlib -T stm32f429.ld -o build/main.elf build/main.o build/main_c.o
 arm-none-eabi-objcopy -O binary build/main.elf build/main.bin
--- main.s
+++ main.s
@@ -4,58 +4,9 @@
 
 .global _start
 
-@ Data Registers
-.equ RCC_AHB1ENR, 0x40023830
-.equ GPIOx_MODER, 0x40021800 
-.equ GPIOx_ODR,   0x40021814
+.extern main
 
-# Vector Table
+@ Vector Table
 .section .isr_vector, "a", %progbits @ Reset Vector
 .word 0x10010000                     @ Stack pointer at top of CCM
-.word _start                         @ Reset Handler
-
-.section .text
-.thumb_func
-_start:
-    @ Enable the clock for GPIOG
-    ldr r0, =RCC_AHB1ENR
-    ldr r1, [r0]
-    orr r1, r1, #(1<<6)
-    str r1, [r0]
-
-    @ Set PG13 to be an output
-    ldr r0, =GPIOx_MODER
-    ldr r1, [r0]
-    bic r1, r1, #(0b11<<26)
-    orr r1, r1, #(0b01<<26)
-    str r1, [r0]
-
-    ldr r1, =GPIOx_ODR
-
-blink_loop:  
-    @ Turn the LED on, finally
-    ldr r2, [r1]
-    orr r2, r2, #(1<<13)
-    str r2, [r1]
-
-    @ Crude delay
-    ldr r0, =1000000
-    bl crude_delay
-
-    @ Turn the LED off.
-    ldr r2, [r1]
-    bic r2, r2, #(1<<13)
-    str r2, [r1]
-
-    @ Second crude delay
-    ldr r0, =1000000
-    bl crude_delay
-
-    b blink_loop
-
-@ r0 - number of cyles to delay execution
-.thumb_func
-crude_delay: 
-    subs r0, r0, #1
-    bne crude_delay
-    bx lr
+.word main                           @ Reset Handler
